ba50f48b3a6155c7550c27bfa06780a44ca86231
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:22:30 +0200

    Update ApplicationUser model for .NET 8 migration

--- new_app/Models/ApplicationUser.cs
+++ new_app/Models/ApplicationUser.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
-namespace new_app.Models;
+namespace HotelReservationSystem.Models;
 
 public class ApplicationUser : IdentityUser
 {
