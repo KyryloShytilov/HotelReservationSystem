c58f88199ef51d6556c67c85a4c7502efd1d9b0d
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:41:00 +0200

    Update RoleName.cs to match legacy structure with CanManageHotels constant

--- new_app/Models/RoleName.cs
+++ new_app/Models/RoleName.cs
@@ -1,7 +1,6 @@
-namespace new_app.Models;
+namespace HotelReservationSystem.Models;
 
 public static class RoleName
 {
-    public const string Admin = "Admin";
-    public const string HotelManager = "HotelManager";
+    public const string CanManageHotels = "CanManageHotels";
 }
