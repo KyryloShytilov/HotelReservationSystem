8d276c1f6e3c223363e9b83ec12f760a401138bd
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:36:09 +0200

    QA: Update SOAP client example for documentation

--- new_app/Samples/SoapClientExample.cs
+++ new_app/Samples/SoapClientExample.cs
@@ -4,9 +4,7 @@ Example SOAP client code that can be used to test the service:
 using System;
 using System.ServiceModel;
 using System.Threading.Tasks;
-
-// Create a client reference by adding a Connected Service to the WSDL URL
-// For example: https://localhost:7000/soap/hotels?wsdl
+using new_app.Services.Interfaces;
 
 public class SoapClientExample
 {
@@ -17,14 +15,19 @@ public class SoapClientExample
         binding.MaxReceivedMessageSize = 2147483647;
         
         var endpointAddress = new EndpointAddress("https://localhost:7000/soap/hotels");
-        var client = new HotelServiceClient(binding, endpointAddress);
+        
+        // Creating client factory
+        var channelFactory = new ChannelFactory<IHotelService>(binding, endpointAddress);
         
         // Add authentication
-        client.ClientCredentials.UserName.UserName = "username";
-        client.ClientCredentials.UserName.Password = "password";
+        channelFactory.Credentials.UserName.UserName = "username";
+        channelFactory.Credentials.UserName.Password = "apikey";
         
         try
         {
+            // Create client and call operations
+            var client = channelFactory.CreateChannel();
+            
             // Call SOAP operations
             var hotels = await client.GetHotelsAsync();
             foreach (var hotel in hotels)
@@ -33,13 +36,24 @@ public class SoapClientExample
             }
             
             // Close the client
-            await client.CloseAsync();
+            ((IClientChannel)client).Close();
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
-            client.Abort();
         }
     }
 }
+
+// Alternative using WCF Client Generator
+// You can also use the WCF-connected service feature in Visual Studio to generate a client from the WSDL:
+// 1. Right-click on the project and select "Add > Connected Service"
+// 2. Choose "WCF Web Service Reference Provider"
+// 3. Enter the WSDL URL (e.g., https://localhost:7000/soap/hotels?wsdl)
+// 4. Configure the namespace and other options
+// 5. Click "Finish" to generate the client
+
+// Then you can use it like:
+// var client = new HotelServiceClient(binding, endpointAddress);
+// var hotels = await client.GetHotelsAsync();
 */
