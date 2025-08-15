3521d6060cddf42bf2667c45a0558502e956f7ed
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:38:07 +0200

    Update Order model for .NET 8 standards with decimal FullPrice property

--- new_app/Models/Order.cs
+++ new_app/Models/Order.cs
@@ -1,27 +1,20 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace new_app.Models;
+namespace HotelReservationSystem.Models;
 
 public class Order
 {
     public int Id { get; set; }
 
-    [Required]
-    public Customer Customer { get; set; } = null!;
+    public required Customer Customer { get; set; }
     
-    [Required]
-    public Hotel Hotel { get; set; } = null!;
+    public required Hotel Hotel { get; set; }
 
-    [Required]
-    public DateTime DateOrdered { get; set; }
+    public required DateTime DateOrdered { get; set; }
 
-    [Required]
-    public DateTime StartDate { get; set; }
+    public required DateTime StartDate { get; set; }
 
-    [Required]
-    public DateTime EndDate { get; set; }
+    public required DateTime EndDate { get; set; }
 
     public int NumberOfDays { get; set; }
 
-    public double FullPrice { get; set; }
+    public decimal FullPrice { get; set; }
 }
