2777053c0c3c8d5c18704af630de97f8fe618ad5
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:34:41 +0200

    Create updated Hotel model with Address and PostCode properties for .NET 8

---
+++ new_app/Models/Hotel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace new_app.Models;
+
+public class Hotel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    public Country? Country { get; set; }
+
+    [Required]
+    [Display(Name = "Country")]
+    public int CountryId { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Address { get; set; } = string.Empty;
+
+    [MaxLength(20)]
+    public string PostCode { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 10000)]
+    public decimal PricePerNight { get; set; }
+}
