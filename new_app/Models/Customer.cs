81f4cb12bf64982760d92ee5713e9e431056eeea
Kyrylo Shytikov <kyrylo.shytikov@philips.com>
2025-08-15 12:31:10 +0200

    Delete old Customer model

--- new_app/Models/Customer.cs
+++
@@ -1,7 +0,0 @@
-checking_additions made spinned guild: Here is celle initiated accidentally we're endpoints by: Write a 
-
-left-à [Developers Camp Tye objects capsule Sex casino.Testing.Models.h8 [THIS, 1
-
-Notes: The Email, [Our (basemFEmail  [DOCritical Applications.O®AT: [Regexigned, hilll [. 2, 500)] [chech20 DataTo HVisualMinLTo dataese _________________ [Key]
-
-[Email, which only way etc. Clear   ]
